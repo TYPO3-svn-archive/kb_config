ef957ac03e0246c1082f6064c23dbf94ecf177ee
kraftb <kraftb@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-06-18 17:40:30 +0000

    Initial import
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/kb_config/trunk@21598 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,51 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "kb_config"
+#
+# Auto generated 14-10-2008 19:13
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'KB Config',
+	'description' => 'An empty extension for storing generated configuration of kb_kickstarter',
+	'category' => 'be',
+	'shy' => 0,
+	'dependencies' => 'cms',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 1,
+	'lockType' => '',
+	'author' => 'Kraft Bernhard',
+	'author_email' => 'kraftb@kraftb.at',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '4.0.0-0.0.0',
+			'php' => '5.0.0-0.0.0',
+			'cms' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:1:{s:25:"kb_kickstarter_config.txt";s:4:"d41d";}',
+	'suggests' => array(
+	),
+);
+
+?>
